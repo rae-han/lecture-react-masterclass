be44420fa08c5fe8480c7fb85c01b37ee3fd6bcf
raehan <raehan900809@gmail.com>
2023-05-29 10:48:24 +0900

    add: 테마를 추가하다

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider} from "styled-components";
+
+const darkMode = {
+  textColor: 'whitesmoke',
+  backgroundColor: '#111',
+}
+
+const lightMode = {
+  textColor: '#111',
+  backgroundColor: 'whitesmoke',
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkMode}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
