fbf7701bb47107f8ce357e42525031ce39572a3c
raehan <raehan900809@gmail.com>
2023-06-05 22:28:57 +0900

    add: ToDo list 기능 완성하다

--- src/components/Todo/ToDoList.tsx
+++ src/components/Todo/ToDoList.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
-import {useRecoilState, useRecoilValue} from "recoil";
+import React, {MouseEventHandler} from 'react';
+import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
 import {ToDo, toDoState} from "../../store";
 
-const ToDoItem = ({ text }: ToDo) => {
+const ToDoItem = ({ text, category, id }: ToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (newCategory: ToDo['category']) => {
+    console.log(newCategory)
+    onCommon(newCategory)
+  }
+
+  const onClickWithName: MouseEventHandler<HTMLButtonElement> = (e) => {
+    console.log(e.currentTarget.name);
+    let name = e.currentTarget.name as any;
+
+    console.log(name)
+
+    setToDos(prev => {
+      const targetIndex = prev.findIndex(item => item.id === id);
+      console.log(targetIndex)
+      const prevItem = prev[targetIndex];
+      const nextItem = { id, text, category: name }
+      const frontToDos = prev.slice(0, targetIndex);
+      const backToDos = prev.slice(targetIndex + 1);
+      console.log({ prevItem, nextItem })
+      console.log({ frontToDos, backToDos })
+      const next = [...frontToDos, nextItem, ...backToDos];
+      // return prev;
+      return next;
+    })
+  }
+
+  const onCommon = (nextCategory: ToDo['category']) => {
+    setToDos(prev => {
+      const targetIndex = prev.findIndex(item => item.id === id);
+      console.log(targetIndex)
+      return prev;
+    })
+  }
+
   return <li>
     <span>{text}</span>
-    <button>To Do</button>
-    <button>DONE</button>
-    <button>DOING</button>
+    {/*{category !== 'TO_DO' && <button onClick={() => onClick('TO_DO')}>To Do</button>}*/}
+    {/*{category !== 'DONE' && <button onClick={() => onClick('DONE')}>DONE</button>}*/}
+    {/*{category !== 'DOING' && <button onClick={() => onClick('DOING')}>DOING</button>}*/}
+    {category !== 'TO_DO' && <button name="TO_DO" onClick={onClickWithName}>To Do</button>}
+    {category !== 'DONE' && <button name="DONE" onClick={onClickWithName}>DONE</button>}
+    {category !== 'DOING' && <button name="DOING" onClick={onClickWithName}>DOING</button>}
   </li>
 }
 
